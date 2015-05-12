4fc8c3a7fb91458152b39a287efc83d1eae1312d
ayoub serti <ayb.serti@gmail.com>
2015-05-12 17:46:55 +0000

    somme error

--- ha_tsdb_engine.cc
+++ ha_tsdb_engine.cc
@@ -692,6 +692,7 @@ int ha_tsdb_engine::create(const char *name, TABLE *table_arg,
 	  // Try to create the file
 		ofh = H5Fcreate(strFilePath.c_str(),H5F_ACC_EXCL,H5P_DEFAULT,H5P_DEFAULT);
 		if(ofh < 0) {
+		  std::cerr << "[INFO]: name:" << name << std::endl;
 			std::cerr << "Error creating TSDB file: '" << strFilePath << "'." << std::endl;
 			DBUG_RETURN(-1);
 		}
